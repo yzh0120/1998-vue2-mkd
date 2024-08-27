5ae8948c5f1c25f7773c60db1de05cd864b93ad1
yzh0120 <644193385@qq.com>
2024-08-27 14:13:23 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
       let devicewidth = document.documentElement.clientWidth //获取当前分辨率下的可是区域宽度
       let scale = devicewidth / 1920 // 分母——设计稿的尺寸
       document.body.style.zoom = scale //放大缩小相应倍数
-    }
+    },
     clientXY() {
       this.$store.commit("setup/windowResize");
     },
